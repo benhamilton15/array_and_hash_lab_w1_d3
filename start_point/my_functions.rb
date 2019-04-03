e2b7f61cdfe5efc9620ecb48bcfe7c1fdad0e964
Ben Hamilton <ben.hamilton@me.com>
2019-04-03 20:48:28 +0100

    created function that finds the sum of values in an array

--- start_point/my_functions.rb
+++ start_point/my_functions.rb
@@ -2,3 +2,11 @@ def add_array_lengths(array1,array2)
   result = array1.length + array2.length
 return result
 end
+
+def sum_array(array)
+  total = 0
+  for number in array
+    total += number
+  end
+return total
+end
